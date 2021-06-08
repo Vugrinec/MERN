176b9ad13bd5b50fe778b7d5bdb1ea289ee1874d
Vugrinec <cenirguv@gmail.com>
2021-06-08 14:31:15 +0200

    imported components

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,6 +1,5 @@
 import PostMessage from '../models/postMessage.js'
 
-
 export const getPosts = async (req, res) => {
     try{
         const postMessage = await PostMessage.find();
@@ -11,6 +10,16 @@ export const getPosts = async (req, res) => {
     }
 }
 
-export const createPost = (req, res) => {
-    res.send('Post Creation');
+export const createPost = async (req, res) => {
+    const post = req.body;
+
+    const newPost = new PostMessage(post);
+
+    try{
+        await newPost.save();
+
+        res.status(201).json(newPost);
+    } catch (error){
+        res.status(409).json({ message: error.message});
+    }
 }
