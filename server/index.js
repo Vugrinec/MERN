37936aeeb80d42da009047d46a100145e2a198b2
Vugrinec <cenirguv@gmail.com>
2021-06-24 10:54:56 +0200

    representing port number

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const CONNECTION_URL = 'mongodb+srv://javascriptmastery:javascriptmastery123@clu
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, () => console.log('Server running on port: ${PORT}')))
+    .then(() => app.listen(PORT, () => console.log('Server running on port:' + PORT)))
     .catch((error) => console.log(error.message));
 
 mongoose.set('useFindAndModify', false);
